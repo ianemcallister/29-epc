22fa112ea24c493d8ad7a79cc706e694c549c578
Ian McAllister <ianmcallister@Ians-Air.home>
2021-07-08 16:02:01 -0700

    Add: Handlebars support for templating

--- server/htmlBuilder/app.js
+++ server/htmlBuilder/app.js
@@ -4,9 +4,22 @@
 */
 
 //  DEFINE DEPENDENCIES
+var fs 		        = require('fs');
+var path 	        = require('path');
+const Handlebars    = require("handlebars");
+
 //  DEFINE MODULE
 var builder = {
+    _loadTemplate: _loadTemplate,
     productPage: productPage
+};
+
+//  PRIVATE METHODS
+function _loadTemplate(filepath) {
+    //  DEFINE LOCAL VARIABLES
+    var readpath = path.join(__dirname, '..', filepath);
+    var file = fs.readFileSync(readpath, 'utf8');
+    return file;
 };
 
 //  DEFINE PUBLIC METHODS
@@ -16,10 +29,12 @@ var builder = {
 */
 function productPage(epcObj) {
     //  DEFINE LOCAL VARIABLES
-    var HTMLPage = "testing testing, 123";
+    var HTMLPage = "";
+    var htmlSource = _loadTemplate('htmlBuilder/templates/landing.htm');
+    var landingTemplate = Handlebars.compile(htmlSource);
 
     //  RETURN
-    return HTMLPage;
+    return landingTemplate(epcObj);
 };
 
 //  EXPORT MODULE
